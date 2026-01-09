913d7c7e301b13dfb9930b488c7e6fb6aeba4470
alfianchii <pianaee379@gmail.com>
2026-01-09 14:41:34 +0700

    chore: fix import

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { Text, ScrollView, StyleSheet } from "react-native"
+import { Text, ScrollView, StyleSheet, View } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
 import { LinearGradient } from "expo-linear-gradient"
 import { colors } from "@theme/colors"
